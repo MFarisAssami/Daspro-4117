5c529d77826ee5fc1717474606466e1fdb511b0a
M Faris Assami <112920146+MFarisAssami@users.noreply.github.com>
2022-09-14 16:55:18 +0700

    Add files via upload

---
+++ latihan02/main.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Muhammad Faris Assami\nNim : A11.2022.14647 \nAsal sekolah : SMAN 2 BALIKPAPAN" << endl;
+    int u=18;
+    float t=1.75;
+    char b='L';
+    string x="Laki-laki";
+
+    cout << "Umur : " << u;
+    cout << "\nTinggi Badan : " << t;
+    cout << "\nUkuran baju : " << b;
+    cout << "\nGender" << x;
+    return 0;
+}
